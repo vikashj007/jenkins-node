4d83661580a60e97ea1d611435aecdac79341d32
harshmathurgeek <81172559+harshmathurgeek@users.noreply.github.com>
2023-10-16 17:49:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ app.get('/',(req,res)=>{
     console.log("hello world");
     res.send("hello from jekins enviorment")
 })
-app.listen(3000,()=>console.log("listening to 3000 port"));
+app.listen(3000,(req,res)=>console.log("listening to 3000 port");res.sendStatus(200););
