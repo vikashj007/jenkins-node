924715cf61b497052be2171569d93175f2469b7e
Vikash Jindal <120328173+vikashj007@users.noreply.github.com>
2023-10-17 10:34:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app.get('/',(req,res)=>{
     console.log("hello world");
     res.send("hello from jekins enviorment")
 })
-app.listen(3000,()=>{
+app.listen(4000,()=>{
     console.log("listening to 3000 port");
     return 0;
 } )
