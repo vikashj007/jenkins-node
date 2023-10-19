e86b9eb88833abecdab97d526a5b0002f0aea847
Vikash Jindal <120328173+vikashj007@users.noreply.github.com>
2023-10-19 11:20:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,7 @@ app.get('/',(req,res)=>{
     console.log("hello world");
     res.send("hello from jekins enviorment")
 })
-app.listen(4000);
+app.listen(4000,()=>{
+    console.log("app is running on 4000");
+          );
     
